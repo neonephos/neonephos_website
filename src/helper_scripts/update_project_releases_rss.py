aaf05a0a414d2176425e32598673ad9db769bbfd
Tristan Scheidemann <tristan.scheidemann@sap.com>
2026-01-30 13:06:05 +0100

    Updated workflow

--- src/helper_scripts/update_project_releases_rss.py
+++ src/helper_scripts/update_project_releases_rss.py
@@ -223,7 +223,8 @@ if not github_token:
 res = fetch_releases_for_orgs(org_list,github_token)
 print(res)
 
-output_path = "dist/feeds/project_releases.xml" 
-os.makedirs(os.path.dirname(output_path), exist_ok=True) 
+output_path = "dist/feed/project/releases.xml"
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
 write_rss_feed(res, output_path)
+
 print("RSS feed written to:", output_path)
