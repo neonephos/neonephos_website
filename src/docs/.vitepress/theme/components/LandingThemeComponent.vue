5c218fe662ee11732fcb8650136ce19b9ea8813c
Tristan Scheidemann <tristan.scheidemann@sap.com>
2026-04-29 15:22:12 +0200

    Update LandingThemeComponent.vue
    Signed-off-by: Tristan Scheidemann <tristan.scheidemann@sap.com>

--- src/docs/.vitepress/theme/components/LandingThemeComponent.vue
+++ src/docs/.vitepress/theme/components/LandingThemeComponent.vue
@@ -31,7 +31,6 @@ import VPHome from 'vitepress/dist/client/theme-default/components/VPHome.vue'
       </div>
     </section>
   </div>
-  <a href="https://kccncind2026.sched.com/event/2IW1x/kubernetes-plane-aerodynamics-breaking-the-architectural-sound-barrier-vasu-chandrasekhara-neonephos-stefan-schimanski-nvidia?iframe=yes&w=100%&sidebar=yes&bg=no") class="kubecon-banner" target="_blank" rel="noopener"> <img :src="withBase('/assets/kubecon_banner_website.svg')" alt="KubeCon+CloudNativeCon India 2026 Banner" /> </a>
   <section class="home-tiles__section">
     <div class="container wrap">
            <LandingTilesThemeComponent
