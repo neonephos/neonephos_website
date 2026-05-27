694c32abc1a23b78f003e4ac5231cd390dcc8760
Tristan Scheidemann <tristan.scheidemann@sap.com>
2026-05-27 14:35:08 +0200

    Update CommitteeThemeComponent.vue
    Signed-off-by: Tristan Scheidemann <tristan.scheidemann@sap.com>

--- src/docs/.vitepress/theme/components/CommitteeThemeComponent.vue
+++ src/docs/.vitepress/theme/components/CommitteeThemeComponent.vue
@@ -29,6 +29,16 @@ const { frontmatter } = useData()
             :features="frontmatter.alternates"
           />
         </section>
+        section v-if="frontmatter.observers">
+          <br>
+          <br>
+          <p class="neonephos-home-hero-subtitle">
+            Observers
+          </p>
+          <CommitteeTilesThemeComponent
+            :features="frontmatter.observers"
+          />
+        </section>
       </section>
 
       <br>
