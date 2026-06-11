cb341dcc190d5e385dfae26bbbb12ffb0d41f414
Tristan Scheidemann <tristan.scheidemann@sap.com>
2026-06-11 14:51:18 +0200

    Update LandingThemeComponent.vue
    Signed-off-by: Tristan Scheidemann <tristan.scheidemann@sap.com>

--- src/docs/.vitepress/theme/components/LandingThemeComponent.vue
+++ src/docs/.vitepress/theme/components/LandingThemeComponent.vue
@@ -202,7 +202,7 @@ import VPHome from 'vitepress/dist/client/theme-default/components/VPHome.vue'
 
 @media screen and (min-width: 700px) {
   .logo-background {
-    background: url('/assets/nn_bg1_temp_compressed.webp') no-repeat center top;
+    background: url('/assets/neonephos_background.svg') no-repeat center top;
     background-size: cover;
     background-color: #002b80;
     padding: 4rem 2rem;
